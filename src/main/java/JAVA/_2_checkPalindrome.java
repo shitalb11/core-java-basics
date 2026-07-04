f8fbf69481de6cfbb41cabc0efac7a76f25e28b9
Shital Bhosale <shitalb075@gmail.com>
2026-07-04 19:12:39 +0530

    new

--- src/main/java/JAVA/_2_checkPalindrome.java
+++ src/main/java/JAVA/_2_checkPalindrome.java
@@ -4,6 +4,6 @@ public class _2_checkPalindrome {
     public static void main(String[] args) {
         String str = "madam";
         String reversed = new StringBuilder(str).reverse().toString();
-
+        System.out.println(str.equals(reversed));
     }
 }
