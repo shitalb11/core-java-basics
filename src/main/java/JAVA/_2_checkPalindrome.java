27127f7a911bb9b0c09477bf28aa99e53ce1f975
Shital Bhosale <shitalb075@gmail.com>
2026-07-04 19:11:57 +0530

    new

---
+++ src/main/java/JAVA/_2_checkPalindrome.java
@@ -0,0 +1,9 @@
+package JAVA;
+
+public class _2_checkPalindrome {
+    public static void main(String[] args) {
+        String str = "madam";
+        String reversed = new StringBuilder(str).reverse().toString();
+
+    }
+}
