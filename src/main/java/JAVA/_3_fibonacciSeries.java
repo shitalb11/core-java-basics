8ed78dc2095b3cb8a1fa9c445c7c9e34380b484c
Shital Bhosale <shitalb075@gmail.com>
2026-07-05 13:15:34 +0530

    new

--- src/main/java/JAVA/_3_fibonacciSeries.java
+++ src/main/java/JAVA/_3_fibonacciSeries.java
@@ -2,6 +2,7 @@ package JAVA;
 
 public class _3_fibonacciSeries {
     public static void main(String[] args) {
-
+        int n=10,num1=0,num2=1;
+        System.out.println("Fibonacci Series: " + num1 + " " + num2);
     }
 }
