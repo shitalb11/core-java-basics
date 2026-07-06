4ece78eab034a157dca1f6703ee721507a361cf6
Shital Bhosale <shitalb075@gmail.com>
2026-07-06 22:31:01 +0530

    new

--- src/main/java/JAVA/_4_FactorialOfNumber.java
+++ src/main/java/JAVA/_4_FactorialOfNumber.java
@@ -1,4 +1,11 @@
 package JAVA;
 
 public class _4_FactorialOfNumber {
+    public static void main(String[] args) {
+        int num = 3, factorial=1;
+        for (int i=1;i<=num;i++){
+            factorial=factorial+i;
+        }
+        System.out.println(factorial);
+    }
 }
