e22f3263f28782535d07fafad1b01df8bdbaf327
Shital Bhosale <shitalb075@gmail.com>
2026-07-07 21:55:27 +0530

    new

--- src/main/java/JAVA/_5_primeNumberCheck.java
+++ src/main/java/JAVA/_5_primeNumberCheck.java
@@ -1,4 +1,15 @@
 package JAVA;
 
 public class _5_primeNumberCheck {
+    public static void main(String[] args) {
+        int num = 11;
+        boolean isPrime = true;
+        for (int i = 2;i<= Math.sqrt(num);i++){
+            if (num % i == 0) {
+                isPrime=false;//divisible, not prime
+                break;//exit early
+            }
+        }
+        System.out.println(isPrime);//true
+    }
 }
