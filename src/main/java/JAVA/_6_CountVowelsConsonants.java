feb6b855f314c72ea989b3048e32b337dd25f03a
Shital Bhosale <shitalb075@gmail.com>
2026-07-08 12:07:54 +0530

    new

--- src/main/java/JAVA/_6_CountVowelsConsonants.java
+++ src/main/java/JAVA/_6_CountVowelsConsonants.java
@@ -1,5 +1,25 @@
 package JAVA;
 
-public class _6_CountVowelsConsonants {
+import java.util.Locale;
 
+public class _6_CountVowelsConsonants {
+    public static void main(String[] args) {
+
+        String str = "Automation";
+
+        int vowels = 0, consonants=0;
+
+        str = str.toLowerCase();
+
+        for (char c: str.toCharArray()){
+
+            if ("aeiou".indexOf(c) != -1){
+                vowels++;
+            } else if (Character.isLetter(c)) {
+              consonants++;
+            }
+        }
+
+        System.out.println("Vowels: "+ vowels + ", Consonants: " + consonants);
+    }
 }
