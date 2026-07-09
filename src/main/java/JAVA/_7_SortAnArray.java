1c6e9981dd2c31d75a49e1b381502b4df0220bda
Shital Bhosale <shitalb075@gmail.com>
2026-07-09 21:08:59 +0530

    new

--- src/main/java/JAVA/_7_SortAnArray.java
+++ src/main/java/JAVA/_7_SortAnArray.java
@@ -1,7 +1,14 @@
 package JAVA;
 
+import java.util.Arrays;
+
 public class _7_SortAnArray {
     public static void main(String[] args) {
+
         int [] arr = {5,6,3,2,9};
+
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+
     }
 }
