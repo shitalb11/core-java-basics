b3fcddaaed5f3f58fed8c159e31e6871187b9409
Shital Bhosale <shitalb075@gmail.com>
2026-07-10 22:04:23 +0530

    new

--- src/main/java/JAVA/_8_MergeTwoArray.java
+++ src/main/java/JAVA/_8_MergeTwoArray.java
@@ -5,6 +5,9 @@ public class _8_MergeTwoArray {
         int [] arr1 = {1,3,5};
         int [] arr2 = {2,4,6};
 
+        int [] merged = new int[arr1.length + arr2.length];
 
+        System.arraycopy(arr1, 0, merged, 0, arr1.length);
+        System.arraycopy(arr2, 0, merged, arr1.length, arr2.length);
     }
 }
