018f6ed3f0776baf4448d50548bed39b7542343d
Shital Bhosale <shitalb075@gmail.com>
2026-07-13 20:29:52 +0530

    new

--- src/main/java/JAVA/_9_LargestElelmentInArray.java
+++ src/main/java/JAVA/_9_LargestElelmentInArray.java
@@ -4,10 +4,15 @@ import java.util.HashSet;
 
 public class _9_LargestElelmentInArray {
     public static void main(String[] args) {
+
         int [] arr = {1,2,3,4,5,6};
-        HashSet<Integer> set = new HashSet<>();
-        for (int num : arr){
-            set.add(num);
+
+        int largest = arr[0];
+
+        for (int i=1;i< arr.length;i++){
+            if (arr[i]>largest){
+                largest=arr[i];
+            }
         }
         System.out.println(largest);
     }
