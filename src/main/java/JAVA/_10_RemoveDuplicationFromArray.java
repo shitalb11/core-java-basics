d06b71267f7d407dc01fd517a4d1622e76b24784
Shital Bhosale <shitalb075@gmail.com>
2026-07-13 20:34:39 +0530

    new

--- src/main/java/JAVA/_10_RemoveDuplicationFromArray.java
+++ src/main/java/JAVA/_10_RemoveDuplicationFromArray.java
@@ -1,5 +1,16 @@
 package JAVA;
 
-public class _10_RemoveDuplicationFromArray {
+import java.util.HashSet;
 
+public class _10_RemoveDuplicationFromArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,5,6};
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int num : arr){
+            set.add(num);
+        }
+        System.out.println(set);
+    }
 }
