9e9e629fd1b2b61db666dde73a081b203d37e595
Shital Bhosale <shitalb075@gmail.com>
2026-07-14 21:05:12 +0530

    new

--- src/main/java/JAVA/_11_CheckArmstrong.java
+++ src/main/java/JAVA/_11_CheckArmstrong.java
@@ -2,6 +2,12 @@ package JAVA;
 
 public class _11_CheckArmstrong {
     public static void main(String[] args) {
-
+        int num = 153,sum = 0,temp = num;
+        while (temp>0){
+            int digit = temp % 10;
+            sum += Math.pow(digit,3);
+            temp /= 10;
+        }
+        System.out.println(sum==num);
     }
 }
--- src/main/java/JAVA/_12_GCDOfTwoNumber.java
+++ src/main/java/JAVA/_12_GCDOfTwoNumber.java
@@ -2,6 +2,12 @@ package JAVA;
 
 public class _12_GCDOfTwoNumber {
     public static void main(String[] args) {
-
+        int a = 60,b = 48;
+        while (b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        System.out.println(a);//GCD
     }
 }
