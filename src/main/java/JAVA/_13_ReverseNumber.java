3e91dafbc41bd2c1774b5d947b5efae2515adb31
Shital Bhosale <shitalb075@gmail.com>
2026-07-16 17:05:59 +0530

    new

--- src/main/java/JAVA/_13_ReverseNumber.java
+++ src/main/java/JAVA/_13_ReverseNumber.java
@@ -1,4 +1,14 @@
 package JAVA;
 
 public class _13_ReverseNumber {
+    public static void main(String[] args) {
+
+        int num = 12345, reversed = 0;
+
+        while (num != 0){
+            reversed = reversed * 10 + num % 10;
+            num=num/10;
+        }
+        System.out.println(reversed); //54321
+    }
 }
