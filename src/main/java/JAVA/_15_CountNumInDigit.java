9b50f0ee7463150f4d53a2f0be4766238c628747
Shital Bhosale <shitalb075@gmail.com>
2026-07-16 17:13:01 +0530

    new

---
+++ src/main/java/JAVA/_15_CountNumInDigit.java
@@ -0,0 +1,9 @@
+package JAVA;
+
+public class _15_CountNumInDigit {
+    public static void main(String[] args) {
+        int num = 1234;
+        int count = String.valueOf(num).length();
+        System.out.println(count);
+    }
+}
