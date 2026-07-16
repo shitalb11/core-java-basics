ba4ec92443d28fa7b0348b00a86d60cb45ead166
Shital Bhosale <shitalb075@gmail.com>
2026-07-16 17:10:55 +0530

    new

--- src/main/java/JAVA/_14_CheckAnagram.java
+++ src/main/java/JAVA/_14_CheckAnagram.java
@@ -1,5 +1,16 @@
 package JAVA;
 
-public class _14_CheckAnagram {
+import java.util.Arrays;
 
+public class _14_CheckAnagram {
+    public static void main(String[] args) {
+        String str1 = "listen", str2 = "silent";
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        System.out.println(Arrays.equals(arr1,arr2));
+    }
 }
