1f1f427b65c6d359ffa04f5bab91e0747b40ca67
Shital Bhosale <shitalb075@gmail.com>
2026-07-20 22:07:34 +0530

    new

--- src/main/java/JAVA/_21_SumOfNumber.java
+++ src/main/java/JAVA/_21_SumOfNumber.java
@@ -1,4 +1,12 @@
 package JAVA;
 
 public class _21_SumOfNumber {
+    public static void main(String[] args) {
+        int num = 12345, sum = 0;
+        while (num != 0){
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println(sum);
+    }
 }
