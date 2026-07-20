4472679705e37534e4ed2f859a3b111d62b2ca0f
Shital Bhosale <shitalb075@gmail.com>
2026-07-20 21:51:18 +0530

    new

--- src/main/java/JAVA/_19_DecimalToBinary.java
+++ src/main/java/JAVA/_19_DecimalToBinary.java
@@ -2,6 +2,8 @@ package JAVA;
 
 public class _19_DecimalToBinary {
     public static void main(String[] args) {
-
+        int num = 10;
+        String binary = Integer.toBinaryString(num);
+        System.out.println(binary);
     }
 }
