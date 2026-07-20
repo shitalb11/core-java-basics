85e441f3aca036a9b139f02ab028d4e9ea33a32e
Shital Bhosale <shitalb075@gmail.com>
2026-07-20 21:54:20 +0530

    ew

---
+++ src/main/java/JAVA/_20_CheckPerfectNumber.java
@@ -0,0 +1,13 @@
+package JAVA;
+
+public class _20_CheckPerfectNumber {
+    public static void main(String[] args) {
+        int num = 28,sum=0;
+        for (int i=1;i<=num/2;i++){
+           if (num%i==0){
+               sum += i;
+           }
+        }
+        System.out.println(num == sum);
+    }
+}
