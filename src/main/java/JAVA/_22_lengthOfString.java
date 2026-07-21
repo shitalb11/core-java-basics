f7d24f760ad672f8b88f8100b8838e5c0ee0cf1f
Shital Bhosale <shitalb075@gmail.com>
2026-07-21 23:40:39 +0530

    new

--- src/main/java/JAVA/_22_lengthOfString.java
+++ src/main/java/JAVA/_22_lengthOfString.java
@@ -2,6 +2,7 @@ package JAVA;
 
 public class _22_lengthOfString {
     public static void main(String[] args) {
-
+        String str = "Shital";
+        System.out.println(str.length());
     }
 }
