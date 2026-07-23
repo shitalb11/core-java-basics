8f3c7a6afbac1d93695bb9120bd2cc011fdd63fc
Shital Bhosale <shitalb075@gmail.com>
2026-07-23 13:34:57 +0530

    new

--- src/main/java/JAVA/_26_FactorialOfNumber.java
+++ src/main/java/JAVA/_26_FactorialOfNumber.java
@@ -2,6 +2,11 @@ package JAVA;
 
 public class _26_FactorialOfNumber {
     public static void main(String[] args) {
-
+        int num = 4;
+        System.out.println(factorial(num));
+    }
+    static int factorial(int n){
+        if(n == 0) return 1;
+        return n * factorial(n - 1);
     }
 }
