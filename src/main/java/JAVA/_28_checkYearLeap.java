f4e0e907abac723e34abb0e7103b6b53706a8c2d
Shital Bhosale <shitalb075@gmail.com>
2026-07-27 23:18:34 +0530

    new

--- src/main/java/JAVA/_28_checkYearLeap.java
+++ src/main/java/JAVA/_28_checkYearLeap.java
@@ -1,4 +1,9 @@
 package JAVA;
 
 public class _28_checkYearLeap {
+    public static void main(String[] args) {
+        int year = 2024;
+        boolean isLeap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+        System.out.println(isLeap);
+    }
 }
